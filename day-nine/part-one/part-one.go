a515653bc39e2a0a9abcc96fc15f961fbfb1278d
Alexandre Mulina <alexandremulina@hotmail.com>
2024-12-28 00:02:12 -0300

    day nine part one

--- day-nine/part-one/part-one.go
+++ day-nine/part-one/part-one.go
@@ -13,45 +13,110 @@ func main() {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	lines := strings.Split(string(content), "\n")
-	fmt.Println("lines", lines)
-	code := lines[0]
-	fmt.Println("leng", len(code))
-	var arrEachMemo [][]int
-	for i := 0; i < len(code); i += 2 {
-		fileLeng, _ := strconv.Atoi(string(code[i]))
-		fileID := i / 2
-		fileSpace := 0
-		if i+1 < len(code) {
-			fileSpace, _ = strconv.Atoi(string(code[i+1]))
-		}
-		arrEachMemo = append(arrEachMemo, []int{fileID, fileLeng, fileSpace})
-		// fmt.Printf("\n File ID %d File Length %d, file space:%d", fileID, fileLeng, fileSpace)
-	}
-	fmt.Println("removeSpaces", arrEachMemo)
-	removeSpaces(arrEachMemo)
 
+	// Remove any whitespace or newlines
+	input := strings.TrimSpace(string(content))
+
+	// Create disk map from input
+	diskMap := createDiskMap(input)
+	fmt.Println("Initial disk map:", diskMap)
+
+	moves := moveFiles(diskMap)
+	checksum := calculateChecksum(moves[len(moves)-1])
+	fmt.Printf("\nFinal Checksum: %d\n", checksum)
 }
 
-func removeSpaces(arrMemo [][]int) {
-	totalSize := 0
-	for _, file := range arrMemo {
-		totalSize += file[1]
-	}
-	fmt.Println("Total size:", totalSize)
+func createDiskMap(input string) string {
+	var result strings.Builder
+	fileID := 0
 
-	result := make([]int, totalSize)
+	// To make the input work
+	digits := strings.Split(input, "")
 
-	currentPos := totalSize - 1
-	for i := len(arrMemo) - 1; i >= 0; i-- {
-		fileID := arrMemo[i][0]
-		fileLength := arrMemo[i][1]
+	for i := 0; i < len(digits); i++ {
+		length, _ := strconv.Atoi(digits[i])
 
-		for j := 0; j < fileLength; j++ {
-			result[currentPos] = fileID
-			currentPos--
+		if i%2 == 0 {
+			for j := 0; j < length; j++ {
+				// Add 'length' number of fileID characters
+				result.WriteRune(rune('0' + fileID))
+			}
+			fileID++
+		} else {
+
+			for j := 0; j < length; j++ {
+				result.WriteRune('.')
+			}
 		}
 	}
 
-	fmt.Println("Final:", result)
+	return result.String()
+}
+
+func moveFiles(diskMap string) []string {
+	result := []string{diskMap}
+	currentMap := diskMap
+
+	for {
+		moved := false
+		chars := []rune(currentMap)
+
+		// Find the last item
+		for i := len(chars) - 1; i > 0; i-- {
+			if chars[i] != '.' {
+
+				for j := 0; j < i; j++ {
+					if chars[j] == '.' {
+						chars[j] = chars[i]
+						chars[i] = '.'
+						moved = true
+						break
+					}
+				}
+				if moved {
+					break
+				}
+			}
+		}
+
+		if !moved {
+			break
+		}
+
+		newMap := string(chars)
+		result = append(result, newMap)
+		currentMap = newMap
+	}
+
+	// Debug output
+	fmt.Println("\nInitial state:")
+	fmt.Println(result[0])
+	fmt.Println("\nFinal state:")
+	fmt.Println(result[len(result)-1])
+
+	return []string{result[0], result[len(result)-1]}
+}
+
+func calculateChecksum(finalMap string) int64 {
+	var checksum int64 = 0
+
+	fmt.Println("\n=== Calculating Checksum ===")
+	fmt.Println("Final Map:", finalMap)
+
+	chars := []rune(finalMap)
+
+	for pos, char := range chars {
+		if char == '.' {
+			continue
+		}
+
+		fileID := int64(char - '0')
+		value := int64(pos) * fileID
+
+		fmt.Printf("Position %d * File ID %d = %d\n", pos, fileID, value)
+		checksum += value
+	}
+
+	fmt.Printf("\nFinal Checksum: %d\n", checksum)
+	return checksum
 }
